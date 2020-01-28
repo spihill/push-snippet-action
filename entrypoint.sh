89f9d3aa55ef0caea860697c308cd61bb0e03877
spihill <spihill.in@gmail.com>
2020-01-28 23:49:07 +0900

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/ash
 
+# $1: user_name
+# $2: library_repo
+# $3: github_token
+
 git config --global user.name "push-snippet-action"
 git config --global user.email "push-snippet-action"
-git clone https://${1}:${3}@github.com/$1/$2.git
+git clone https://${1}:${3}@github.com/${1}/${2}.git
+cd ${2}
+ls -l
