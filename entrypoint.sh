65e919010028fb6007ae67ead11e7ead59cf78df
spihill <spihill.in@gmail.com>
2020-01-28 23:59:51 +0900

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,7 @@
 git config --global user.name "push-snippet-action"
 git config --global user.email "push-snippet-action"
 git clone https://${1}:${3}@github.com/${1}/${2}.git
+ls -l
+cp make_cpp_json.py ${2}
 cd ${2}
 python3 make_cpp_json.py
