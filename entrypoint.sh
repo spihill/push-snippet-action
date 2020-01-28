89d6482eda8f064ce3f36b922bafff0789e070a7
spihill <spihill.in@gmail.com>
2020-01-29 00:18:25 +0900

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,21 @@
 # $1: user_name
 # $2: library_repo
 # $3: github_token
+# $4: snippet_gist_id
+# $5: snippet_gist_token
 
 git config --global user.name "push-snippet-action"
 git config --global user.email "push-snippet-action"
+
 git clone https://${1}:${3}@github.com/${1}/${2}.git
-ls / -l
+
 cp /make_cpp_json.py ${2}
 cp /resolve_includes.py ${2}
-cd ${2}
-python3 make_cpp_json.py
+
+cd ${2} && python3 make_cpp_json.py && cd ..
+
+git clone https://spihill:${5}@gist.github.com/spihill/${4} && rm ./${4} -f
+
+mv ./${2}/cpp.json ./${4} && cd ./${4}
+git add cpp.json
+git commit -m "updated by push-snippet-action" && git push origin HEAD || echo "NOT changed"
