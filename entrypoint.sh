9badd33b5bf4482391078e489cc4c8cf5aa1a619
spihill <spihill.in@gmail.com>
2020-01-28 23:47:06 +0900

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/ash
 
-git clone https://github.com/$1/$2.git
+git config --global user.name "push-snippet-action"
+git config --global user.email "push-snippet-action"
+git clone https://${1}:${3}@github.com/$1/$2.git
